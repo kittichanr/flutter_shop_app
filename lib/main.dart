7cd3fc94e4644d0479c8a19e94ebede653772f3c
Petch <petchkubb@live.com>
2020-10-04 06:27:17 +0700

    connect multi provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-
 import 'package:provider/provider.dart';
 
 import './screens/product_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 
 import './providers/products.dart';
+import './providers/cart.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,8 +14,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (cotext) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         home: ProductOverviewScreen(),
         theme: ThemeData(
