ed8d97b1b37977d6b89335ea79937453278c6718
Petch <petchkubb@live.com>
2020-10-17 10:08:09 +0700

    add edit page

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import './screens/product_detail_screen.dart';
 import './screens/cart_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
+import './screens/edit_product_screen.dart';
 
 import './providers/products.dart';
 import './providers/cart.dart';
@@ -51,6 +52,7 @@ class MyApp extends StatelessWidget {
           CartScreen.routeName: (context) => CartScreen(),
           OrdersScreen.routeName: (context) => OrdersScreen(),
           UserProductsScreen.routeName: (context) => UserProductsScreen(),
+          EditProductScreen.routeName: (context) => EditProductScreen(),
         },
       ),
     );
--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
+
+import '../screens/edit_product_screen.dart';
 import '../widgets/user_product_item.dart';
 import '../widgets/app_drawer.dart';
 
@@ -13,7 +15,13 @@ class UserProductsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Products'),
-        actions: [IconButton(icon: const Icon(Icons.add), onPressed: () {})],
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditProductScreen.routeName);
+              })r
+        ],
       ),
       drawer: AppDrawer(),
       body: Padding(
